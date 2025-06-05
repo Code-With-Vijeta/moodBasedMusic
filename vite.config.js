36e4513a758b938687be300937a4c35fed5262b7
Code-With-Vijeta <codevijeta@gmail.com>
2025-06-05 20:47:07 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: 'frontend',            
   plugins: [react()],
-  base: '/moodBasedMusic/',    
-  build: {
-    outDir: '../dist',        
-    emptyOutDir: true
-  }
+  base: "/moodBasedMusic/" 
 })
